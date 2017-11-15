6a44fb334cdb6bafc6bc0b58ec71dc44c55b97e4
Asil Çetin <asilcetin@users.noreply.github.com>
2017-11-15 18:57:19 +0100

    basic folder structure

---
+++ config.rb
@@ -0,0 +1,17 @@
+http_path = "/" #root level target path
+css_dir = "dist/css" #targets our default style.css
+sass_dir = "scss" #targets our scss directory
+images_dir = "assets/images" #targets our pre existing image directory
+javascripts_dir = "dist/js" #targets our JavaScript directory
+ 
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+output_style = :nested
+
+# To enable relative paths to assets via compass helper functions.
+# note: this is important in wordpress themes for sprites
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors.
+line_comments = false
