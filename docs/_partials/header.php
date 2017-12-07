1776aa7f63b2d9e5e55539ac4cb89400aa733a6b
Asil Çetin <asilcetin@users.noreply.github.com>
2017-12-07 16:36:07 +0100

    unused icon font removed

--- docs/_partials/header.php
+++ docs/_partials/header.php
@@ -9,9 +9,6 @@
 
     <title>Fundament</title>
 
-    <!-- Material icons font -->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
     <!-- Fontawesome icons -->
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 
